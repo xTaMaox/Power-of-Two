9028bffa310056241b2ad3a62675214b7f3a0b2f
Muhamad Adi Prasetyo <88260025+xTaMaox@users.noreply.github.com>
2023-02-07 16:18:34 +0700

    Add files via upload

---
+++ Power of Two.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        // Base cases as '1' is the only odd number which is a power of 2 i.e. 2^0...
+        if(n==1)
+            return true;
+        // All other odd numbers are not powers of 2...
+        else if (n % 2 != 0 || n == 0)
+            return false;
+        // Recursive function call
+        return isPowerOfTwo(n/2);
+    }
+}
